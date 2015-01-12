3c28e925fa386e9652c826fe699cd5fc638c1e03
kuteken <kuteken@gmail.com>
2015-01-13 03:12:41 +0700

    nested describe

--- spec/basic_spec.rb
+++ spec/basic_spec.rb
@@ -1,10 +1,16 @@
 RSpec.describe "four arithmetic operations" do
-  it '1 + 1 is 2' do
-    expect(1 + 1).to eq 2
+  describe 'addition' do
+    it '1 + 1 is 2' do
+      expect(1 + 1).to eq 2
+    end
   end
-  it '10 - 1 is 9' do
-    expect(10 - 1).to eq 9
+
+  describe 'subtraction' do
+    it '10 - 1 is 9' do
+      expect(10 - 1).to eq 9
+    end
   end
+
   it 'all can' do
     expect(1 + 2).to  eq 3
     expect(3 - 4).to  eq -1
