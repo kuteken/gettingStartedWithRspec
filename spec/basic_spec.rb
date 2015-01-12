2d559622435d9b7cb5f08913cea09d53f38ddd7b
kuteken <kuteken@gmail.com>
2015-01-13 03:04:53 +0700

    many expectation in a example

--- spec/basic_spec.rb
+++ spec/basic_spec.rb
@@ -5,4 +5,10 @@ RSpec.describe "four arithmetic operations" do
   it '10 - 1 is 9' do
     expect(10 - 1).to eq 9
   end
+  it 'all can' do
+    expect(1 + 2).to  eq 3
+    expect(3 - 4).to  eq -1
+    expect(5 * 6).to  eq 30
+    expect(8 / 2).to  eq 4
+  end
 end
