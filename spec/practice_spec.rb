25e167d790a9072feb0b9981446b48f78b71294d
kuteken <kuteken@gmail.com>
2015-01-13 20:18:35 +0700

    replace user by let

--- spec/practice_spec.rb
+++ spec/practice_spec.rb
@@ -2,13 +2,13 @@ require_relative '../lib/practice'
 
 RSpec.describe User do
   describe '#greet' do
+    let(:user) { User.new(params) }
     let(:params) { { name: 'たろう' } }
     context 'When under 12' do
       before do
         params.merge!(age: 12)
       end
       it 'answer Hiragana' do
-        user = User.new(params)
         expect(user.greet).to eq 'ぼくはたろうだよ。'
       end
     end
@@ -17,7 +17,6 @@ RSpec.describe User do
         params.merge!(age: 13)
       end
       it 'answer Kanji' do
-        user = User.new(params)
         expect(user.greet).to eq '僕はたろうです。'
       end
     end
