d76ab6cf44364f21bf4abaa90f5bbe78e4ffdb74
kuteken <kuteken@gmail.com>
2015-01-13 20:37:09 +0700

    remove lengthy test code

--- spec/practice_spec.rb
+++ spec/practice_spec.rb
@@ -5,28 +5,14 @@ RSpec.describe User do
     let(:user) { User.new(name: 'たろう', age: age) }
     subject { user.greet }
 
-    shared_examples 'children greeting' do
-      it { is_expected.to eq 'ぼくはたろうだよ。' }
-    end
-    context 'When under 0' do
-      let(:age) { 0 }
-      it_behaves_like 'children greeting'
-    end
     context 'When under 12' do
       let(:age) { 12 }
-      it_behaves_like 'children greeting'
+      it { is_expected.to eq 'ぼくはたろうだよ。' }
     end
 
-    shared_examples 'adult greeting' do
-      it { is_expected.to eq '僕はたろうです。' }
-    end
     context 'When upper 13' do
       let(:age) { 13 }
-      it_behaves_like 'adult greeting'
-    end
-    context 'When upper 100' do
-      let(:age) { 100 }
-      it_behaves_like 'adult greeting'
+      it { is_expected.to eq '僕はたろうです。' }
     end
   end
 end
