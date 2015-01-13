f54132199f50c0f170860509b78c530202159e1f
kuteken <kuteken@gmail.com>
2015-01-13 20:23:49 +0700

    replaced by subject

--- spec/practice_spec.rb
+++ spec/practice_spec.rb
@@ -4,16 +4,17 @@ RSpec.describe User do
   describe '#greet' do
     let(:user) { User.new(params) }
     let(:params) { { name: 'たろう', age: age } }
+    subject { user.greet }
     context 'When under 12' do
       let(:age) { 12 }
       it 'answer Hiragana' do
-        expect(user.greet).to eq 'ぼくはたろうだよ。'
+        is_expected.to eq 'ぼくはたろうだよ。'
       end
     end
     context 'When upper 13' do
       let(:age) { 13 }
       it 'answer Kanji' do
-        expect(user.greet).to eq '僕はたろうです。'
+        is_expected.to eq '僕はたろうです。'
       end
     end
   end
