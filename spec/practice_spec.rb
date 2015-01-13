988e27102c8a21cb39de0d34503ce420a82146b8
kuteken <kuteken@gmail.com>
2015-01-13 20:20:24 +0700

    replace age by let

--- spec/practice_spec.rb
+++ spec/practice_spec.rb
@@ -3,19 +3,15 @@ require_relative '../lib/practice'
 RSpec.describe User do
   describe '#greet' do
     let(:user) { User.new(params) }
-    let(:params) { { name: 'たろう' } }
+    let(:params) { { name: 'たろう', age: age } }
     context 'When under 12' do
-      before do
-        params.merge!(age: 12)
-      end
+      let(:age) { 12 }
       it 'answer Hiragana' do
         expect(user.greet).to eq 'ぼくはたろうだよ。'
       end
     end
     context 'When upper 13' do
-      before do
-        params.merge!(age: 13)
-      end
+      let(:age) { 13 }
       it 'answer Kanji' do
         expect(user.greet).to eq '僕はたろうです。'
       end
