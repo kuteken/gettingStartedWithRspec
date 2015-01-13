009264d919acc7f7abe2857fd59a11f5a3c43d40
kuteken <kuteken@gmail.com>
2015-01-13 20:44:25 +0700

    add test for child function

--- spec/practice_spec.rb
+++ spec/practice_spec.rb
@@ -1,18 +1,38 @@
 require_relative '../lib/practice'
 
 RSpec.describe User do
-  describe '#greet' do
-    let(:user) { User.new(name: 'たろう', age: age) }
-    subject { user.greet }
+  let(:user) { User.new(name: 'たろう', age: age) }
+  shared_context 'When under 12' do
+    let(:age) { 12 }
+  end
+  shared_context 'When upper 13' do
+    let(:age) { 13 }
+  end
 
+  describe '#greet' do
+    subject { user.greet }
     context 'When under 12' do
-      let(:age) { 12 }
+      include_context 'When under 12'
       it { is_expected.to eq 'ぼくはたろうだよ。' }
     end
 
     context 'When upper 13' do
-      let(:age) { 13 }
+      include_context 'When upper 13'
       it { is_expected.to eq '僕はたろうです。' }
     end
   end
+
+  describe "#child?" do
+    subject { user.child? }
+    context 'When under 12' do
+      include_context 'When under 12'
+      it { is_expected.to eq true }
+    end
+
+    context 'When upper 13' do
+      include_context 'When upper 13'
+      it { is_expected.to eq false }
+    end
+  end
+
 end
