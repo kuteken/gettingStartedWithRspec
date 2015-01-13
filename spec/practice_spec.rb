7cee0698ea6429dba71e2b472c6eb3ef173ff2e2
kuteken <kuteken@gmail.com>
2015-01-13 20:10:09 +0700

    groupde by before in context

--- spec/practice_spec.rb
+++ spec/practice_spec.rb
@@ -6,14 +6,20 @@ RSpec.describe User do
       @params = { name: 'たろう' }
     end
     context 'When under 12' do
+      before do
+        @params.merge!(age: 12)
+      end
       it 'answer Hiragana' do
-        user = User.new(@params.merge(age: 12))
+        user = User.new(@params)
         expect(user.greet).to eq 'ぼくはたろうだよ。'
       end
     end
     context 'When upper 13' do
+      before do
+        @params.merge!(age: 13)
+      end
       it 'answer Kanji' do
-        user = User.new(@params.merge(age: 13))
+        user = User.new(@params)
         expect(user.greet).to eq '僕はたろうです。'
       end
     end
