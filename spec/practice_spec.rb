a01a6ecb967180bbd597fede8b6c152a129d1057
kuteken <kuteken@gmail.com>
2015-01-13 20:33:13 +0700

    use shared_examples

--- spec/practice_spec.rb
+++ spec/practice_spec.rb
@@ -4,13 +4,29 @@ RSpec.describe User do
   describe '#greet' do
     let(:user) { User.new(name: 'たろう', age: age) }
     subject { user.greet }
+
+    shared_examples 'children greeting' do
+      it { is_expected.to eq 'ぼくはたろうだよ。' }
+    end
+    context 'When under 0' do
+      let(:age) { 0 }
+      it_behaves_like 'children greeting'
+    end
     context 'When under 12' do
       let(:age) { 12 }
-      it { is_expected.to eq 'ぼくはたろうだよ。' }
+      it_behaves_like 'children greeting'
+    end
+
+    shared_examples 'adult greeting' do
+      it { is_expected.to eq '僕はたろうです。' }
     end
     context 'When upper 13' do
       let(:age) { 13 }
-      it { is_expected.to eq '僕はたろうです。' }
+      it_behaves_like 'adult greeting'
+    end
+    context 'When upper 100' do
+      let(:age) { 100 }
+      it_behaves_like 'adult greeting'
     end
   end
 end
